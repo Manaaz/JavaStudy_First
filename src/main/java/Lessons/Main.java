a8dcbf04bc4950a8f937acb95be7f60140e0f0ac
Sergey <73065172+Manaaz@users.noreply.github.com>
2021-11-30 01:12:36 +0300

    Java, Lesson 2.
    Complete 1-5.

---
+++ src/main/java/Lessons/Main.java
@@ -0,0 +1,16 @@
+package Lessons;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Lesson2 lesson2 = new Lesson2();
+
+        lesson2.lesson2(1);
+        lesson2.lesson2(2);
+        lesson2.lesson2(3);
+        lesson2.lesson2(4);
+        lesson2.lesson2(5);
+
+    }
+}
